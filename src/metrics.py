Bug fixes for rotation.

src/metrics.py
@@ -7,13 +7,13 @@ def report_CFL(my_server, client_list):
         clients_accs = []
         for client in client_cluster_list : 
             print('Client ID : ', client.id)
-            print('Client rotation : ', client.rotation)
+            #print('Client rotation : ', client.rotation)
             print('Cluster model accuracy on client test data')
             acc = test_model(my_server.clusters_models[cluster_id], client.data_loader['test'])*100
             clients_accs.append(acc)
             print(acc)
         print("Cluster accuracy : ", np.mean(clients_accs))
         print("Cluster accuracy std : ", np.std(clients_accs))
-        cluster_rot = [client.rotation for client in client_cluster_list]
-        values, counts = np.unique(cluster_rot, return_counts = True)
-        print('Cluster rotations : ' , {values[i] : counts[i] for i in range(len(values))} )
+        #cluster_rot = [client.rotation for client in client_cluster_list]
+        #values, counts = np.unique(cluster_rot, return_counts = True)
+        #print('Cluster rotations : ' , {values[i] : counts[i] for i in range(len(values))} )

src/utils_data.py
@@ -68,7 +68,7 @@ def rotate_images(client,rotation):
         for img in images:
             rotated_img = np.rot90(img, k=rotation//90)  # Rotate image by specified angle
             rotated_images.append(rotated_img)   
-            client.data['x'] = rotated_images
+        client.data['x'] = np.array(rotated_images)
 
 def data_preparation(client):
     from sklearn.model_selection import train_test_split
@@ -130,14 +130,14 @@ def setup_experiment_labelswap(number_of_clients,number_of_samples_by_clients, m
             data_preparation(client)
     return my_server, clientlist
 
-def setup_experiment_quantity_skew(model,number_of_client=200,number_of_max_samples=100,skewlist=[1,0.5,0.1,0.05,0.01], seed = 42):
+def setup_experiment_quantity_skew(model,number_of_client=200,number_of_max_samples=100,skewlist=[1,0.5,0.25,0.1,0.05], seed = 42):
     number_of_skew = len(skewlist)
-    number_of_client_by_skew = number_of_client // number_of_skew 
-    clientdata = [data_distribution(number_of_client_by_skew,number_of_max_samples*skew,seed) for skew in skewlist]        
+    number_of_client_by_skew = number_of_client//number_of_skew 
+    clientdata = [data_distribution(number_of_client_by_skew,int(number_of_max_samples*skew),seed) for skew in skewlist]        
     clientlist = []
     for id in range(number_of_client_by_skew):
         for skew_id in range(len(skewlist)):
-            client = Client(id*range(len(skewlist))+ skew_id,clientdata[skew_id][id])
+            client = Client(id*len(skewlist)+ skew_id,clientdata[skew_id][id])
             setattr(client,'quantity_skew',skewlist[skew_id])
             clientlist.append(client)
     for client in clientlist : 

src/utils_fed.py
@@ -276,9 +276,11 @@ def fed_training_plan_on_shot_k_means(my_server, client_list,rounds_before_clust
         print('Aggregating local models with FedAVG !')
         fedavg(my_server, client_list)
         print('Communication round {} completed !'.format(round+1))
+    '''
+    if need of saving models 
     for cluster_id in range(4): 
         torch.save(my_server.clusters_models[cluster_id].state_dict(), 'model_{}.pth'.format(cluster_id))
-        
+    '''    
 def calculate_cluster_id(my_server,client_list, number_of_clusters=4):
     from src.models import MnistNN, SimpleLinear
     from src.utils_training import loss_calculation
@@ -293,39 +295,9 @@ def calculate_cluster_id(my_server,client_list, number_of_clusters=4):
         client.cluster_id = index_of_min_loss
     listofcluster = [client.cluster_id for client in client_list]
     return listofcluster
-'''
-def client_fedavg(my_server, client_list):
-    """
-    Implementation of the FedAvg Algorithm with cluster-based averaging.
-    """
-    for cluster_id in range(my_server.num_clusters):
-        print('FedAVG on cluster {}!'.format(cluster_id))
-        # Filter clients belonging to the current cluster
-        cluster_client_list = [client for client in client_list if client.cluster_id == cluster_id]
-        print('Number of clients in cluster {}: {}'.format(cluster_id, len(cluster_client_list)))
-        
-        # Initialize a dictionary to store parameter sums
-        param_sums = {name: torch.zeros_like(param.data) for name, param in cluster_client_list[0].model.named_parameters()}
-        
-        # Sum up parameters from all clients
-        total_clients = len(cluster_client_list)
-        for client in cluster_client_list:
-            for name, param in client.model.named_parameters():
-                param_sums[name] += param.data
-        
-        # Average parameters
-        averaged_params = {name: param_sum / total_clients for name, param_sum in param_sums.items()}
-        
-        # Create a new model and load averaged parameters
-        new_model = copy.deepcopy(cluster_client_list[0].model)  # Assuming all models have the same architecture
-        for name, param in new_model.named_parameters():
-            param.data = averaged_params[name]
-        
-        # Store the new model in the server's cluster models
-        my_server.clusters_models[cluster_id] = new_model
-'''
-            
+
 def init_server_cluster(my_server,client_list, number_of_clusters=4, seed = 42):
+    # Set client to random cluster for first round
     from src.models import MnistNN, SimpleLinear
     from src.utils_training import loss_calculation
     import numpy as np
@@ -333,7 +305,9 @@ def init_server_cluster(my_server,client_list, number_of_clusters=4, seed = 42):
     torch.manual_seed(seed)
     my_server.clusters_models = {cluster_id: SimpleLinear(h1=200) for cluster_id in range(number_of_clusters)} 
     for client in client_list:
-        #print('Calculating all cluster model loss on local data for client {} !'.format(client.id))
+        client.cluster_id = np.random.randint(0,number_of_clusters)
+        
+    '''
         cluster_losses = []
         for cluster_id in range(number_of_clusters):
             cluster_loss = loss_calculation(my_server.clusters_models[cluster_id], client.data_loader['train'])
@@ -351,6 +325,7 @@ def init_server_cluster(my_server,client_list, number_of_clusters=4, seed = 42):
             my_server.clusters_models[cluster_id] = SimpleLinear(200)
             listofcluster = calculate_cluster_id(my_server,client_list, number_of_clusters=4)
             print('update', listofcluster)
+    '''
             
 def set_client_cluster(my_server,client_list,number_of_clusters=4,epochs=10):
     from src.utils_training import loss_calculation
@@ -365,20 +340,7 @@ def set_client_cluster(my_server,client_list,number_of_clusters=4,epochs=10):
         print('Best loss with cluster model {}'.format(index_of_min_loss))
         client.model = copy.deepcopy(my_server.clusters_models[index_of_min_loss])
         client.cluster_id = index_of_min_loss
-'''     
-def client_side_clustering(my_server,client_list,number_of_clusters=4,epochs=10): 
-    import numpy as np
-    from src.utils_training import loss_calculation
-    for client in client_list:
-        print('Training all cluster model on local data for client {} !'.format(client.id))
-        model_and_loss = [train_model(copy.deepcopy(my_server.clusters_models[cluster_id]), client.data_loader['train'], client.data_loader['test'],epochs, learning_rate=0.001) for cluster_id in range(4)]
-        losses = [loss_calculation(model, client.data_loader['train']) for model, _ in model_and_loss]
-        index_of_min_loss = np.argmin(losses)
-        print('Best loss with cluster model {}'.format( index_of_min_loss))
-        clientmodel, _ = model_and_loss[index_of_min_loss]
-        client.model = copy.deepcopy(clientmodel)
-        client.cluster_id = index_of_min_loss 
-'''
+
         
 def fed_training_plan_client_side(my_server, client_list,rounds=3, epoch=10, number_of_clusters = 4,lr = 0.001,initcluster = True):
     """

trainCFLserver.py
@@ -13,7 +13,7 @@ print(torch.__version__)
 import src.config
 from src.models import MnistNN, SimpleLinear 
 from src.fedclass import Client, Server
-from src.utils_data import setup_experiment_rotation, centralize_data
+from src.utils_data import setup_experiment_rotation, setup_experiment_labelswap, setup_experiment_quantity_skew, centralize_data
 from src.utils_training import train_model, test_model
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,7 +23,7 @@ from src.metrics import report_CFL
 
 # Load config from JSON file
 lr = 0.1
-with open('config.json') as config_file:
+with open('serverconfig.json') as config_file:
     config_data = json.load(config_file)
 
 experiments = config_data['experiments']
@@ -35,13 +35,12 @@ for exp_id, experiment in enumerate(experiments):
         seed = config['seed']
         number_of_clients = config['number_of_clients']
         number_of_samples_of_each_labels_by_clients = config['number_of_samples_of_each_labels_by_clients']
-        centralized_model_epochs = config['centralized_model_epochs']
-        federated_rounds = config['federated_rounds']
-        federated_local_epochs = config['federated_local_epochs']
         cfl_before_cluster_rounds = config['cfl_before_cluster_rounds']
         cfl_after_cluster_rounds = config['cfl_after_cluster_rounds']
         cfl_local_epochs = config['cfl_local_epochs']
+        number_of_clusters = config['number_of_clusters']
         output = config['output']
+        heterogeneity = config["heterogeneity"]
         
         print(config)  # Print current configuration
         
@@ -50,40 +49,24 @@ for exp_id, experiment in enumerate(experiments):
 
 
     torch.manual_seed(seed)
-
-        # CONFIG VARIABLE 
     model = SimpleLinear()
-    my_server, client_list  = setup_experiment_rotation(number_of_clients,number_of_samples_of_each_labels_by_clients,model)   
-    train_loader, test_loader = centralize_data(client_list)
-
-    centralized_model = train_model(copy.deepcopy(model), train_loader, test_loader,centralized_model_epochs,learning_rate= lr ) 
-
-    from src.utils_training import train_model
-
-    from src.utils_fed import fed_training_plan, send_server_model_to_client
-    fed_training_plan(my_server, client_list, federated_rounds, federated_local_epochs,lr= lr)
-    for client in client_list : 
-        print('For client {} test data we have :'.format(client.id))
-        print("Accuracy: {:.2f}%".format(test_model(my_server.model, client.data_loader['test'])*100))
-    test_fed  = test_model(my_server.model, test_loader)
-    test_central = test_model(centralized_model, test_loader)
-
-
     from src.utils_fed import fed_training_plan_on_shot_k_means
-    my_server, client_list  = setup_experiment_rotation(number_of_clients,number_of_samples_of_each_labels_by_clients,model)   
-    fed_training_plan_on_shot_k_means(my_server,client_list, cfl_before_cluster_rounds , cfl_after_cluster_rounds , cfl_local_epochs,lr= lr)
+    if heterogeneity == 'concept_shift_on_features':
+        my_server, client_list  = setup_experiment_rotation(number_of_clients,number_of_samples_of_each_labels_by_clients,model)   
+    elif heterogeneity == 'concept_shift_on_labels':
+        my_server, client_list  = setup_experiment_labelswap(number_of_clients,number_of_samples_of_each_labels_by_clients,model,number_of_cluster=number_of_clusters,seed=seed)
+    elif heterogeneity == 'quantity_skew':
+        my_server, client_list  = setup_experiment_quantity_skew(model,number_of_client= number_of_clients, number_of_max_samples= number_of_samples_of_each_labels_by_clients,seed = seed)
+        
+    fed_training_plan_on_shot_k_means(my_server,client_list, cfl_before_cluster_rounds , cfl_after_cluster_rounds , cfl_local_epochs,lr= lr, number_of_clusters=number_of_clusters)
     
     with open("./{}.txt".format(output), 'w') as f:
         with contextlib.redirect_stdout(src.config.Tee(f, sys.stdout)):    
             print('server num clusters : ', my_server.num_clusters)
-            print('Federated Model Accuracy')
-            print("Accuracy: {:.2f}%".format(test_fed*100))
-            print('centralized model Accuracy')
-            print("Accuracy: {:.2f}%".format(test_central*100))
-            for cluster_id in range(4):
+            for cluster_id in range(number_of_clusters):
                 client_list_cluster = [client for client in client_list if client.cluster_id == cluster_id]
                 _, test_loader = centralize_data(client_list_cluster)
                 print('Federated Model Accuracy for cluster', cluster_id)
                 print("Accuracy: {:.2f}%".format(test_model(my_server.clusters_models[cluster_id], test_loader)*100))
             print(config)
-            report_CFL(my_server,client_list)
+            #report_CFL(my_server,client_list)