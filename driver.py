415f064add6b26ae22fc647bee4dc516f5db3505
Omar El Rifai <omar.void@gmail.com>
2024-08-01 15:16:38 +0200

    Use click for argument parsing in driver

--- driver.py
+++ driver.py
@@ -1,5 +1,20 @@
 
-def main_driver():
+import click
+
+@click.command()
+@click.option('--exp_type', help="The experiment type to run")
+@click.option('--heterogeneity_type', help="The data heterogeneity to test (or dataset)")
+@click.option('--num_clients', type=int)
+@click.option('--num_samples_by_label', type=int)
+@click.option('--num_clusters', type=int)
+@click.option('--centralized_epochs', type=int, default=50)
+@click.option('--federated_rounds', type=int, default=5)
+@click.option('--federated_local_epochs', type=int, default=20)
+@click.option('--seed', type=int, default=42)
+
+
+
+def main_driver(exp_type, heterogeneity_type, num_clients, num_samples_by_label, num_clusters, centralized_epochs, federated_rounds, federated_local_epochs, seed):
 
     from pathlib import Path
     import pandas as pd
@@ -9,43 +24,37 @@ def main_driver():
 
     setup_logging()
 
-    df_experiments = pd.read_csv("exp_configs.csv")
-
     df_results = pd.DataFrame(columns=['exp_type','training_type', 'heterogeneity_type', 'model_type', 'heterogeneity_class', 'accuracy'])
 
-    for _, row_exp in df_experiments.iterrows():
-
-        break_experiement = False
-
-        output_name =  row_exp.to_string(header=False, index=False, name=False).replace(' ', "").replace('\n','_')
-
-        hash_outputname = get_uid(output_name)
-
-        pathlist = Path("results").rglob('*.json')
+    row_exp = pd.Series({"exp_type": exp_type, "heterogeneity_type": heterogeneity_type, "num_clients": num_clients,
+               "num_samples_by_label": num_samples_by_label, "num_clusters": num_clusters, "centralized_epochs": centralized_epochs,
+               "federated_rounds": federated_rounds, "federated_local_epochs": federated_local_epochs, "seed": seed})
     
-        for file_name in pathlist:
+
+    output_name =  row_exp.to_string(header=False, index=False, name=False).replace(' ', "").replace('\n','_')
+
+    hash_outputname = get_uid(output_name)
+
+    pathlist = Path("results").rglob('*.json')
+
+    for file_name in pathlist:
+
+        if get_uid(str(file_name.stem)) == hash_outputname:
+
+            cprint(f"Experiment {str(file_name.stem)} already executed in with results in \n {output_name}.json", lvl="warning")   
+        
+            return 
+    try:
+        
+        model_server, list_clients, list_heterogeneities = setup_experiment(row_exp)
     
-            if get_uid(str(file_name.stem)) == hash_outputname:
+    except Exception as e:
 
-                cprint(f"Experiment {str(file_name.stem)} already executed in with results in \n {output_name}.json", lvl="warning")   
-            
-                break_experiement = True
-        
-        if break_experiement:
-        
-            continue
-        
-        try:
-            
-            model_server, list_clients, list_heterogeneities = setup_experiment(row_exp)
-        
-        except Exception as e:
+        cprint(f"Could not run experiment with parameters {output_name}. Exception {e}")
 
-            cprint(f"Could not run experiment with parameters {output_name}. Exception {e}")
-            
-            continue
-
-        launch_experiment(model_server, list_clients, list_heterogeneities, row_exp, df_results, output_name)
+        return 
+    
+    launch_experiment(model_server, list_clients, list_heterogeneities, row_exp, df_results, output_name)
 
     return          
             
